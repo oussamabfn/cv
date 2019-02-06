155c9f4b68ce8ae369e72bfb106c607715d75872
oussamabfn <46899136+oussamabfn@users.noreply.github.com>
2019-02-06 23:26:54 +0100

    Delete Oussama-cv.js

--- Oussama-cv.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", function() { 
-var deplacement = ((window.scrollY+ window.innerHeight)/document.body.offsetHeight )*100;
-document.getElementById('progress_bar').value = deplacement;});
-function myScroll() { 
-var progress = document.getElementById('progress_bar').value;
-progress = progress+(document.body.offsetHeight-window.innerHeight)/200;
-window.scroll(0,progress);}
